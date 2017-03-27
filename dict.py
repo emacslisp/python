f0a92530f1560f29f1d703081e6a52e2523f1bab
emacslisp <wudi850620@hotmail.com>
2017-03-28 10:21:25 +1100

    dict on command line

---
+++ dict.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+# searchString = input from command line argv[0]
+# finalUrl = "http://www.dict.org/bin/Dict?Form=Dict1&Query=" + searchString + "&Strategy=*&Database=*";//"http://dict.cn/mini.php?q=" + searchString;
+# htmlString = getStringFromUrl(finalUrl);
+# textString = html2text(htmlString);
+# print textString;
+
+
