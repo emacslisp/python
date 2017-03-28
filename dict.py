aa159682625fd1a2e5facba346fb3c70096dbbe0
emacslisp <wudi850620@hotmail.com>
2017-03-28 14:45:30 +1100

    url to string

--- dict.py
+++ dict.py
@@ -6,4 +6,10 @@
 # textString = html2text(htmlString);
 # print textString;
 
-
+import urllib
+search = 'search'
+link = "http://www.dict.org/bin/Dict?Form=Dict1&Query=" + search + "&Strategy=*&Database=*";
+#"http://dict.cn/mini.php?q=" + searchString
+f = urllib.urlopen(link)           
+myfile = f.read()  
+print myfile
