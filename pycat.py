4c000ef6eb61634f26ef768a62aaba1852540141
Eddy Wu <java.gdb@gmail.com>
2019-08-06 11:06:28 +1000

    #pycat command line

---
+++ pycat.py
@@ -0,0 +1,16 @@
+import sys
+
+def func():
+    devArray = sys.argv
+
+    if len(devArray) > 2:
+        print('pycat <file>')
+        return
+
+    FileName = devArray[1]
+    with open(FileName) as f:
+        ewText=f.read()
+        print(ewText)
+
+
+func()
