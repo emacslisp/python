52727a1b05bfced6f28ae373c8e2ca8a85133db9
ewu <java.gdb@gmail.com>
2024-04-13 23:36:09 +1000

    frequencies Of Elements in python

---
+++ leetcode/LinkedListFrequency.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from typing import Optional
+
+from leetcode import ListNode
+
+class Solution:
+    def frequenciesOfElements(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        p = head
+        dict = {}
+        while p is not None :
+            if dict.get(p.val) is None:
+                dict[p.val] = 0
+
+            dict[p.val] = 1 + dict[p.val]
+            p = p.next
+
+        result = ListNode(0)
+        p = result
+        for key, value in dict.items():
+            node = ListNode(value)
+            p.next = node
+            p = p.next
+
+        return result.next
+
+
+
---
+++ test/numpyTest.py
@@ -0,0 +1,9 @@
+# Load library
+import numpy as np
+
+# Create a matrix
+matrix = np.array([[1, 2],
+[1, 2],
+[1, 2]])
+
+print(matrix);
